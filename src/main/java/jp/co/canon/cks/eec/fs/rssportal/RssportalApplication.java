e21da36f99e3663d7594748d94e36e3a895a4ec4
ted <tykim2@canon-bs.co.kr>
2020-04-08 08:40:43 +0900

    DB연동중

--- src/main/java/jp/co/canon/cks/eec/fs/rssportal/RssportalApplication.java
+++ src/main/java/jp/co/canon/cks/eec/fs/rssportal/RssportalApplication.java
@@ -1,7 +1,14 @@
 package jp.co.canon.cks.eec.fs.rssportal;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class RssportalApplication {
@@ -10,4 +17,18 @@ public class RssportalApplication {
         SpringApplication.run(RssportalApplication.class, args);
     }
 
+//    @Bean
+//    public SqlSessionFactory sqlSessionFactory(DataSource ds) throws Exception {
+//        SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
+//        factory.setDataSource(ds);
+//        factory.setMapperLocations(
+//                new PathMatchingResourcePatternResolver().getResource("classpath*:sql/**/*.xml")
+//        );
+//        return factory.getObject();
+//    }
+//
+//    @Bean
+//    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory factory) {
+//        return new SqlSessionTemplate(factory);
+//    }
 }
