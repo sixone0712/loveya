b22f197276a1c91512c22e23de5d3076df8a20e5
dean <sixone0712@gmail.com>
2020-05-11 16:47:18 +0900

    자동다운로드 리덕스 연결

--- src/main/webapp/js/components/Auto/CheckSetting.js
+++ src/main/webapp/js/components/Auto/CheckSetting.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import * as autoPlanActions from "../../modules/autoPlan";
 import { Col, FormGroup, Label } from "reactstrap";
+import moment from "moment";
+import * as DEFINE from "../../define";
 
 class RSSautoplanchecksetting extends Component {
     render() {
+        const { autoPlan, toolInfoListCheckCnt, logInfoListCheckCnt } = this.props;
+        const { planId, collectType, interval, intervalUnit, from, to, collectStart, description } = autoPlan.toJS();
+
         return (
             <div className="form-section checksetting">
                 <Col className="pd-0">
@@ -16,33 +24,38 @@ class RSSautoplanchecksetting extends Component {
                         <FormGroup className="auto-plan-checklist-form-group">
                             <FormGroup>
                                 <Label>Plan ID</Label>
-                                <div className="setting-info">Plan 1</div>
+                                <div className="setting-info">{planId}</div>
                             </FormGroup>
                             <FormGroup>
                                 <Label>Period</Label>
                                 <div className="setting-info">
-                                    2020-04-20 00:00 ~ 2020-04-27 23:59
+                                    {moment(from).format("YYYY-MM-DD HH:mm")} ~ {moment(to).format("YYYY-MM-DD HH:mm")}
                                 </div>
                             </FormGroup>
                             <FormGroup>
                                 <Label>Start</Label>
-                                <div className="setting-info">2020-04-29 00:00</div>
+                                <div className="setting-info">{moment(collectStart).format("YYYY-MM-DD HH:mm")}</div>
                             </FormGroup>
                             <FormGroup>
                                 <Label>Mode</Label>
-                                <div className="setting-info">Continuous</div>
+                                <div className="setting-info">
+                                    { collectType == DEFINE.AUTO_MODE_CONTINUOUS
+                                        ? "Continous"
+                                        : `Cycle / ${interval} ${intervalUnit}`
+                                    }
+                                </div>
                             </FormGroup>
                             <FormGroup>
                                 <Label>Machine</Label>
-                                <div className="setting-info">3 Machines</div>
+                                <div className="setting-info">{toolInfoListCheckCnt} Machines</div>
                             </FormGroup>
                             <FormGroup>
                                 <Label>Target</Label>
-                                <div className="setting-info">2 Targets</div>
+                                <div className="setting-info">{logInfoListCheckCnt} Targets</div>
                             </FormGroup>
                             <FormGroup>
                                 <Label>Description</Label>
-                                <div className="setting-info">Default collection plan</div>
+                                <div className="setting-info">{description}</div>
                             </FormGroup>
                         </FormGroup>
                     </div>
@@ -52,4 +65,15 @@ class RSSautoplanchecksetting extends Component {
     }
 }
 
-export default RSSautoplanchecksetting;
+export default connect(
+    (state) => ({
+        autoPlan: state.autoPlan.get('autoPlan'),
+        toolInfoList: state.viewList.get('toolInfoList'),
+        logInfoList: state.viewList.get('logInfoList'),
+        toolInfoListCheckCnt: state.viewList.get('toolInfoListCheckCnt'),
+        logInfoListCheckCnt: state.viewList.get('logInfoListCheckCnt'),
+    }),
+    (dispatch) => ({
+        autoPlanActions: bindActionCreators(autoPlanActions, dispatch),
+    })
+)(RSSautoplanchecksetting);
