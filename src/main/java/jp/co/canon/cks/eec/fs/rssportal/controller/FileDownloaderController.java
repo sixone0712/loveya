347402f5d64c587f541defb81a3badc343b56d68
ted <tykim2@canon-bs.co.kr>
2020-05-26 16:41:37 +0900

    test&debug/ FileDownloaderController 테스트케이스 작성

--- src/main/java/jp/co/canon/cks/eec/fs/rssportal/controller/FileDownloaderController.java
+++ src/main/java/jp/co/canon/cks/eec/fs/rssportal/controller/FileDownloaderController.java
@@ -47,6 +47,7 @@ public class FileDownloaderController {
             log.warn("no target to download");
             return null;
         }
+        param.forEach((key, value)->log.info("key="+key+"\nvalue="+value));
 
         Map<String, Map<String, DownloadForm>> map = new HashMap<>();
         List<Map<String, Object>> downloadList = (List<Map<String, Object>>) param.get("list");
@@ -107,7 +108,7 @@ public class FileDownloaderController {
             @RequestParam(value="dlId", defaultValue="") String dlId,
             HttpServletRequest request,
             HttpServletResponse response) {
-        log.info(String.format("request \"%s?dlId=%d\"", request.getServletPath(), dlId));
+        log.info(String.format("request \"%s?dlId=%s\"", request.getServletPath(), dlId));
         if(dlId.isEmpty()) {
             log.error("invalid param");
             return null;
@@ -146,7 +147,7 @@ public class FileDownloaderController {
     @ResponseBody
     public ResponseEntity<String> cancelDownload(HttpServletRequest request,
                                                  @RequestParam(value="dlId") String downloadId) {
-        log.info(String.format("request \"%s?dlId=%d\"", request.getServletPath(), downloadId));
+        log.info(String.format("request \"%s?dlId=%s\"", request.getServletPath(), downloadId));
         if(!fileDownloader.cancelRequest(downloadId))
             return new ResponseEntity("invalid download id", HttpStatus.NOT_FOUND);
         return new ResponseEntity("ok", HttpStatus.OK);
