9b203ea4a6fac98af8b66ca49e70d6c6c6a5ab10
ymkwon1203 <60598576+ymkwon1203@users.noreply.github.com>
2020-05-26 20:52:59 +0900

    2020/05/26 PlanList 관련 주석 처리 커밋 롤백

--- src/main/webapp/js/components/Auto/PlanList.js
+++ src/main/webapp/js/components/Auto/PlanList.js
@@ -464,8 +464,7 @@ function CreateDetail(detail) {
         case detailType.REGISTERED:
             component = (<><FontAwesomeIcon className="completed" icon={faRegistered} /> Registered</>);   break;
         case detailType.COLLECTING:
-            break;
-            /*component = (<><ClockLoader size={15} color={"rgb(47, 158, 68)"} css={spinnerStyles}/> Collecting</>);   break;*/
+            component = (<><ClockLoader size={15} color={"rgb(47, 158, 68)"} css={spinnerStyles}/> Collecting</>);   break;
         case detailType.COLLECTED:
             component = (<><FontAwesomeIcon className="completed" icon={faCheck} /> Collected</>);   break;
         case detailType.SUSPENDED:
