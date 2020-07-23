ee97cb65a04cf647e774ff4b4392b0ee752b2d42
강태룡 <trkang@canon-bs.co.kr>
2020-07-23 17:26:30 +0900

    예전 rssportal 하위의 VFTP 관련 구현/테스트 삭제.

--- src/main/java/jp/co/canon/cks/eec/fs/rssportal/vftp/service/ftp/SubRequest.java
+++
@@ -1,7 +0,0 @@
-package jp.co.canon.cks.eec.fs.rssportal.vftp.service.ftp;
-
-import jp.co.canon.cks.eec.util.ftp.FTP;
-
-public abstract class SubRequest {
-    public abstract void processRequest(FtpWorker worker, FTP ftp) throws Exception;
-}
