1df13726dfeeb08addf6cb0707f512bdad6e9855
Dean <sixone0712@gmail.com>
2020-07-28 16:01:03 +0900

    CmdController Rest API 수정

--- src/main/java/jp/co/canon/cks/eec/fs/rssportal/controller/CmdController.java
+++ src/main/java/jp/co/canon/cks/eec/fs/rssportal/controller/CmdController.java
@@ -1,153 +1,171 @@
 package jp.co.canon.cks.eec.fs.rssportal.controller;
 
+import jp.co.canon.cks.eec.fs.rssportal.Defines.RSSErrorReason;
+import jp.co.canon.cks.eec.fs.rssportal.model.error.RSSError;
 import jp.co.canon.cks.eec.fs.rssportal.service.CommandService;
-import jp.co.canon.cks.eec.fs.rssportal.session.SessionContext;
 import jp.co.canon.cks.eec.fs.rssportal.vo.CommandVo;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/rss/rest/cmd")
+@RequestMapping("/rss/api/vftp/command")
 public class CmdController {
-
-    private final HttpSession httpSession;
-    private final CommandService serviceCmd;
-    private final String CMD_RESULT = "result";
-    private final String CMD_DATA = "data";
-
     @Autowired
-    public CmdController(HttpSession httpSession, CommandService serviceCmd) {
-        log.info("/command/Controller");
-        this.httpSession = httpSession;
-        this.serviceCmd = serviceCmd;
-    }
-
-
-    @GetMapping("/getList")
-    @ResponseBody
-    public Map<String, Object> getCmdList(@RequestParam Map<String, Object> param)  throws Exception {
-        int res = 0;
-        log.info("/getList");
-        String type = param.containsKey("cmd_type")?(String)param.get("cmd_type"):null;
-        Map<String, Object> returnData = new HashMap<>();
-
-        if(type==null || type.isEmpty())
-        {
-            returnData.put(CMD_RESULT,  201);
-            log.info("COMMAND_FAIL_EMPTY_NAME");
-        }
-        else
-        {
-            List<CommandVo> list = serviceCmd.getCommandList(type);
-            log.info("List size is  "+list.size());
-
-            if(list == null || list.size() == 0 )
-            {
-                returnData.put(CMD_RESULT,  1);
-                log.info("List data is null");
-            }
-            else {
-                returnData.put(CMD_RESULT,  0);
-                returnData.put(CMD_DATA, list);
-            }
-        }
-        return returnData;
-    }
-
-    @GetMapping("/add")
-    @ResponseBody
-    public int addCmd(@RequestParam Map<String, Object> param)  throws Exception {
-        int res = 0;
-        log.info("/addCmd");
-        String type = param.containsKey("cmd_type")?(String)param.get("cmd_type"):null;
-        String name = param.containsKey("cmd_name")?(String)param.get("cmd_name"):null;
-
-        if(type==null || type.isEmpty() || name==null || name.isEmpty())
-        {
-            res = 201; //COMMAND_FAIL_EMPTY_NAME
-            log.info("COMMAND_FAIL_EMPTY_NAME");
-        }
-        else
-        {
-            CommandVo returnObj = serviceCmd.findCommand(name,type);
-            if(returnObj != null )
-            {
-                log.info("already exist command");
-                res = 200; //COMMAND_FAIL_SAME_NAME
-            }
-            else
-            {
-                Boolean result = false;
-                CommandVo cmdObj = new CommandVo();
-                cmdObj.setCmd_type(type);
-                cmdObj.setCmd_name(name);
-                result= serviceCmd.addCmd(cmdObj);
-                if(result) {
-                    log.info("add new Command success");
-                }
-                else
-                {
-                    res = 1;
-                    log.info("add new Command fail");
-                }
-            }
-        }
-        return res;
-    }
-
-    @GetMapping("/delete")
-    @ResponseBody
-    public int deleteCmd(@RequestParam Map<String, Object> param)  throws Exception {
-        int res = 0;
-        log.info("/delete command");
-        String id = param.containsKey("id")?(String)param.get("id"):null;
-        log.info("id" + id);
-
-        if(id==null || id.isEmpty())
-        {
-            res = 203; //COMMAND_NO_SELECT_COMMAND
-            log.info("COMMAND_NO_SELECT_COMMAND");
-        }
-        else
-        {
-            CommandVo cmdObj = new CommandVo();
-            CommandVo returnObj = serviceCmd.getCommand(Integer.parseInt(id));
-            if(returnObj != null )
-            {
-                Boolean result = false;
-
-                result = serviceCmd.deleteCmd(Integer.parseInt(id));
-                if(result) {
-                    log.info("delete Command success");
-                }
-                else
-                {
-                    res = 1;
-                    log.info("delete Command fail");
-                }
-            }
-            else
-            {
-                log.info("COMMAND_NO_SUCH_COMMAND");
-                res = 202; //COMMAND_NO_SUCH_COMMAND
-            }
-        }
-        return res;
-    }
-
-
+    CommandService serviceCmd;
 
     private final Log log = LogFactory.getLog(getClass());
 
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<?> getCmdList(HttpServletRequest request,
+                                        @RequestParam(name="type", required = false, defaultValue = "") String type) throws Exception {
+        log.info(String.format("[Get] %s", request.getServletPath()));
+        Map<String, Object> resBody = new HashMap<>();
+        RSSError error = new RSSError();
+        List<CommandVo> list = null;
+        if(type == null || type.isEmpty()) list = serviceCmd.getCommandListAll();
+        else list = serviceCmd.getCommandList(type);
+
+        if(list == null) {
+            error.setReason(RSSErrorReason.INTERNAL_ERROR);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resBody);
+        }
+
+        resBody.put("lists", list);
+        return ResponseEntity.status(HttpStatus.OK).body(resBody);
+    }
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<?> addCmd(HttpServletRequest request,
+                                    @RequestBody Map<String, Object> param)  throws Exception {
+        log.info(String.format("[Post] %s", request.getServletPath()));
+        Map<String, Object> resBody = new HashMap<>();
+        RSSError error = new RSSError();
+
+        String type = param.containsKey("cmd_type") ? (String)param.get("cmd_type") : null;
+        String name = param.containsKey("cmd_name") ? (String)param.get("cmd_name") : null;
+
+        if(type == null || type.isEmpty() || name == null || name.isEmpty())
+        {
+            error.setReason(RSSErrorReason.INVALID_PARAMETER);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resBody);
+        }
+
+        if(!type.equals("vftp_compat") && !type.equals("vftp_ftp")) {
+            error.setReason(RSSErrorReason.INVALID_PARAMETER);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resBody);
+        }
+
+        CommandVo findCommand = serviceCmd.findCommand(name, type);
+        if(findCommand != null) {
+            error.setReason(RSSErrorReason.DUPLICATE_USER);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resBody);
+        }
+
+        CommandVo newCommand = new CommandVo();
+        newCommand.setCmd_type(type);
+        newCommand.setCmd_name(name);
+        if(!serviceCmd.addCmd(newCommand)) {
+            error.setReason(RSSErrorReason.INTERNAL_ERROR);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resBody);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<?> deleteCmd(HttpServletRequest request,
+                                       @PathVariable("id") String id) {
+        log.info(String.format("[Delete] %s", request.getServletPath()));
+        Map<String, Object> resBody = new HashMap<>();
+        RSSError error = new RSSError();
+
+        if(id == null || id.isEmpty())
+        {
+            error.setReason(RSSErrorReason.NOT_FOUND);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(resBody);
+        }
+
+        int commandId = Integer.parseInt(id);
+        CommandVo findCommand = serviceCmd.getCommand(commandId);
+        if(findCommand == null) {
+            error.setReason(RSSErrorReason.NOT_FOUND);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(resBody);
+        }
+
+        if(serviceCmd.deleteCmd(commandId)) {
+            error.setReason(RSSErrorReason.INTERNAL_ERROR);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resBody);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<?> modifyCmd(HttpServletRequest request,
+                                       @PathVariable("id") String id,
+                                       @RequestBody Map<String, Object> param) {
+        log.info(String.format("[Put] %s", request.getServletPath()));
+        Map<String, Object> resBody = new HashMap<>();
+        RSSError error = new RSSError();
+
+        if(id == null || id.isEmpty()) {
+            error.setReason(RSSErrorReason.NOT_FOUND);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(resBody);
+        }
+
+        if(param == null) {
+            log.error("no param");
+            error.setReason(RSSErrorReason.INVALID_PARAMETER);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resBody);
+        }
+
+        String cmdName = param.containsKey("cmd_name") ? (String) param.get("cmd_name") : null;
+
+        if(cmdName == null || cmdName.isEmpty()) {
+            error.setReason(RSSErrorReason.INVALID_PARAMETER);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resBody);
+        }
+
+        int commandId = Integer.parseInt(id);
+        CommandVo findCommand = serviceCmd.getCommand(commandId);
+        if(findCommand == null) {
+            error.setReason(RSSErrorReason.NOT_FOUND);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(resBody);
+        }
+
+        findCommand.setCmd_name(cmdName);
+        if(serviceCmd.modifyCmd(findCommand)) {
+            error.setReason(RSSErrorReason.INTERNAL_ERROR);
+            resBody.put("error", error.getRSSError());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resBody);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
 }
