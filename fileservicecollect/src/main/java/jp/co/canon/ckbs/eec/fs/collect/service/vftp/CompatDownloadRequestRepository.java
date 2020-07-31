1cc0a4a65c11c4db94e1733fc605f22878295b20
강태룡 <trkang@canon-bs.co.kr>
2020-07-31 14:21:48 +0900

    불필요 import 삭제.

--- fileservicecollect/src/main/java/jp/co/canon/ckbs/eec/fs/collect/service/vftp/CompatDownloadRequestRepository.java
+++ fileservicecollect/src/main/java/jp/co/canon/ckbs/eec/fs/collect/service/vftp/CompatDownloadRequestRepository.java
@@ -1,7 +1,6 @@
 package jp.co.canon.ckbs.eec.fs.collect.service.vftp;
 
 import jp.co.canon.ckbs.eec.fs.collect.model.VFtpCompatDownloadRequest;
-import jp.co.canon.ckbs.eec.fs.collect.model.VFtpSssDownloadRequest;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
